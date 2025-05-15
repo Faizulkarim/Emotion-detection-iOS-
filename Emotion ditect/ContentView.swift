d83af4e21407a51779b016114349f83a995c458f
BSFaizul <md.faizul@brainstation-23.com>
2025-05-15 12:45:55 +0600

    Initial Commit

---
+++ Emotion ditect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Emotion ditect
+//
+//  Created by Faizul 1825 on 15/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Emotion ditect/Emotion_ditectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emotion_ditectApp.swift
+//  Emotion ditect
+//
+//  Created by Faizul 1825 on 15/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Emotion_ditectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
